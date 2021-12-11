2b0f47561075dc47ac2dedef86295d9e9ca4d2ad
DanielyanVK <danielyanvk@gmail.com>
2021-12-11 12:40:59 +0300

    Summary

---
+++ ButtonXib/HomeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HomeViewController.swift
+//  ButtonXib
+//
+//  Created by Vladislav on 11.12.2021.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    // Label outlet
+    @IBOutlet weak var countingLabel: UILabel!
+    var count = 0
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    // Plus Button Logic
+    
+    @IBAction func plusCountButton() {
+        count += 1
+        countingLabel.text = String(count)
+    }
+    
+    // Minus Button Logic
+    @IBAction func minusCountButton() {
+        count -= 1
+        countingLabel.text = String(count)}
+    
+}
